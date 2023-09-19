c939f92fc5e51b160aeebe5381b7869faab66e76
macielhenrique <jhenriquefmaciel@gmail.com>
2023-09-19 00:45:51 -0300

    Primeiro repositorio

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,51 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    // Listar todos os registros
+
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    // Obter um registro
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not Found
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
+  }
+
+  store() {
+    // criar novo Registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  async delete(request, response) {
+    // deletar um registro
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+    // 204: No Content
+    response.sendStatus(204);
+  }
+}
+
+// Singleton
+module.exports = new ContactController();
